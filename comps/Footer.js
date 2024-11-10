8dfddba8161bc26bcf4134a0f6f10a87695d0469
jude <judeorifa28@gmail.com>
2024-11-10 03:06:12 +0100

    initial commit

---
+++ comps/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        Copyright &copy; 2024  Ninja Lia
+        
+    </footer>
+  )
+}
+
+export default Footer 
---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Head from 'next/head'
+
+const About = () => {
+  return (
+    <>
+    <Head>
+    <title>Ninja List | About</title>
+    <meta name="keywords" content="ninjas" />
+  </Head>
+    <div>
+        <h1>About</h1>
+        <p>If you want to handle multiple roles, you can simply extend the check. For example, if you want both admin and manager to have access:</p>
+    </div>
+    </>
+  )
+}
+
+export default About
